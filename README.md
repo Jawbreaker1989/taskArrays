29b90ab0d32a485385634794a8801b0aca2efeaf
Jairo Armando <jairo.armando@gmail.com>
2023-11-22 08:42:10 -0500

    VersioInicial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Ejemplo de Arreglos
+
+Proyecto académico para los estudiantes de Programación de la [uptc](https://wwww.uptc.edu.co) usando `Java`
+
+## Herramientas
+ 
+- IntelliIdea
+- JDK 1.8
+
+> Se construye con el aporte de los estudiantes y se usa 
---
+++ src/logic/ServiceArrays.java
@@ -0,0 +1,52 @@
+package logic;
+
+/**
+ * Clase que gestiona un arreglo de valores enteros
+ */
+public class ServiceArrays {
+
+    //Arreglo
+    private int[] numbers;
+    //variable que controla la posición del siguiente elemento
+    private int position;
+
+    public static final int BURBBLE = 0;
+
+    /**
+     * Constructor vació
+     */
+    public ServiceArrays() {
+        //TODO Instanciar el arreglo de un tamaño de 10 elemetos
+    }
+
+    /**
+     * Constructor que inicializa el arreglo
+     * @param size tamaño del arreglo
+     */
+    public ServiceArrays(int size) {
+        //TODO Instanciar el arreglo de un tamaño del parámetro
+        this.position = position;
+    }
+
+    public ServiceArrays(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    /**
+     * Permite cargar los valores del arreglo
+     * @param begin
+     * @param end
+     */
+    public void loadElements(int begin, int end ){
+
+    }
+
+    public void addElement( int element){
+
+    }
+
+    private int[] sureCapacity(){
+
+        return null;
+    }
+}
