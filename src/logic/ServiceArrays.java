92b42e4ae87268328c82f25b59db6ed97b9db52a
Cesar Caro <143824917+Jawbreaker1989@users.noreply.github.com>
2023-11-25 21:00:11 -0500

    Add sample functionality
    Update print method
    
    Incorporate new elements into the array
    
    Optimize ServiceArrays initialization
    
    Refactor showNumbers method
    
    Document the usage of the ServiceArrays class
    
    Add a test case with a predefined array
    
    Improve console output in the main method

--- src/logic/ServiceArrays.java
+++ src/logic/ServiceArrays.java
@@ -1,52 +1,164 @@
 package logic;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * Clase que gestiona un arreglo de valores enteros
  */
 public class ServiceArrays {
-
-    //Arreglo
+    // Arreglo0
     private int[] numbers;
-    //variable que controla la posición del siguiente elemento
+    // variable que controla la posición del siguiente elemento
     private int position;
+    public static final int BUBBLE = 0;
+    public static final int SELECTION = 1;
+    public static final int INSERTION = 2;
+    public static final int SHELL = 3;
 
-    public static final int BURBBLE = 0;
+    private static final int SIZE_DEFAULT = 10;
 
     /**
-     * Constructor vació
+     * Constructor vacio
      */
-    public ServiceArrays() {
-        //TODO Instanciar el arreglo de un tamaño de 10 elemetos
+    public ServiceArrays(int beggin, int end) {
+        // TODO Instanciar el arreglo de un tamaño de 10 elemetos
+        numbers = new int[SIZE_DEFAULT];
+        loadElements(10, 999);
+        position = SIZE_DEFAULT;
     }
 
     /**
      * Constructor que inicializa el arreglo
+     * 
      * @param size tamaño del arreglo
      */
-    public ServiceArrays(int size) {
-        //TODO Instanciar el arreglo de un tamaño del parámetro
-        this.position = position;
+    public ServiceArrays(int size, int beggin, int end) {
+        // TODO Instanciar el arreglo de un tamaño del parámetro
+        numbers = new int[size];
+        loadElements(10, 999);
+        position = size;
     }
 
+    /**
+     *
+     * @param numbers
+     */
     public ServiceArrays(int[] numbers) {
         this.numbers = numbers;
+        position = numbers.length;
     }
 
     /**
      * Permite cargar los valores del arreglo
+     * 
      * @param begin
      * @param end
      */
-    public void loadElements(int begin, int end ){
+    private void loadElements(int begin, int end) {
+        int min = begin <= end ? begin : end;
+        int max = begin >= end ? begin : end;
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = new Random().nextInt(max - min + 1) + min;
+        }
+    }
+
+    public int[] getNumbers() {
+        return numbers.clone();
+    }
+
+    /**
+     *
+     * @param element
+     */
+    public void addElement(int element) {
+        if (position >= numbers.length) {
+            numbers = sureCapacity();
+        }
+        numbers[position] = element;
+        position++;
 
     }
 
-    public void addElement( int element){
+    /**
+     *
+     * @return
+     */
+    private int[] sureCapacity() {
+        int newSize = numbers.length + numbers.length / 2;
+        int[] newArray = Arrays.copyOf(numbers, newSize);
 
+        return newArray;
     }
 
-    private int[] sureCapacity(){
-
-        return null;
+    /**
+     *
+     * @return
+     */
+    public String showNumbers() {
+        StringBuffer sb = new StringBuffer("[");
+        for (int i = 0; i < numbers.length; i++) {
+            sb.append(numbers[i] + ",");
+        }
+        String aux = sb.substring(0, sb.length() - 1);
+        aux += "]";
+        return aux.toString();
     }
+
+    /**
+     *
+     * @param element
+     * @return
+     */
+    public int findElement(int element) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == element) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     *
+     * @param size
+     * @return
+     */
+
+    public int getMin() {
+        int min = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+        return min;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getMax() {
+        int max = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        return max;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getSum() {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
+    }
+
 }
--- src/view/Runner.java
+++ src/view/Runner.java
@@ -1,7 +1,28 @@
 package view;
 
+import logic.ServiceArrays;
+
+import java.util.Arrays;
+
 public class Runner {
     public static void main(String[] args) {
+        ServiceArrays serviceArrays = new ServiceArrays(300, 505);
+
+        System.out.println(serviceArrays.showNumbers());
+
+        int[] numbers = serviceArrays.getNumbers();
+
+        System.out.println(Arrays.toString(numbers));
+
+        serviceArrays.addElement(567);
+        serviceArrays.addElement(43);
+        serviceArrays.addElement(64);
+
+        System.out.println(" show " + serviceArrays.showNumbers());
+        System.out.println(Arrays.toString(serviceArrays.getNumbers()));
+        ServiceArrays sa = new ServiceArrays(new int[] { 43, 64, 68 });
+        System.out.println(Arrays.toString(sa.getNumbers()));
+        System.out.println(sa.findElement(68));
 
     }
 }
